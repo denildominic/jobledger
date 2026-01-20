18768f37e053a29408d5fd67c40dfcccefb4e842
Denil Dominic <denildominic11@gmail.com>
2026-01-19 19:21:22 -0600

    Copyright addition

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="border-t border-slate-200 dark:border-slate-800">
       <div className="container py-10 text-sm opacity-80">
-        © {new Date().getFullYear()} Denil Dominic. All rights reserved.
+        © {new Date().getFullYear()} JobLedger. All rights reserved.
       </div>
     </footer>
   );
