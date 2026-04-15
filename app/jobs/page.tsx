c1332d5a40a2493ae9283182a82f59a161c4390d
Denil Dominic <denildominic11@gmail.com>
2026-04-14 23:02:41 -0500

    Minor changes

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -256,7 +256,7 @@ export default function JobsPage() {
         );
         break;
       default:
-        arr = [...arr]; // relevant (API order)
+        arr = [...arr]; 
     }
 
     return arr;
